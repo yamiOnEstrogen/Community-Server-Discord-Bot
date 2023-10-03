a8da47d926cf01e2f38d07865c6edecfbc88f52a
ステラ <live.hylia@outlook.com>
2023-10-03 18:55:54 -0400

    Remove `LevelUp` embed after `5` seconds

--- src/events/messageCreate.ts
+++ src/events/messageCreate.ts
@@ -10,65 +10,71 @@ const supportServer = process.env.guild_id;
 const staffRole = process.env.STAFF_ID;
 
 module.exports = {
-    name: Events.MessageCreate,
-    once: false,
-    async execute(message: Message, client: IClient) {
-        if (message.author.bot) return;
+  name: Events.MessageCreate,
+  once: false,
+  async execute(message: Message, client: IClient) {
+    if (message.author.bot) return;
 
-        if (message.content.startsWith(config.StaffPrefix)) {
-            const server = client.guilds.cache.get(supportServer as string);
-            const member = await server?.members.fetch(message.author.id);
-            if (!(member?.roles.cache.has(staffRole as string))) {
-              return;
-            }
-      
-            const args = message.content.slice(config.StaffPrefix.length).trim().split(/ +/);
-            const commandName = args.shift()?.toLowerCase();
+    if (message.content.startsWith(config.StaffPrefix)) {
+      const server = client.guilds.cache.get(supportServer as string);
+      const member = await server?.members.fetch(message.author.id);
+      if (!(member?.roles.cache.has(staffRole as string))) {
+        return;
+      }
 
-      
-            if (!commandName) {
-              return;
-            }
-      
-            const command = client.messageCommands.get(commandName);
- 
+      const args = message.content.slice(config.StaffPrefix.length).trim().split(/ +/);
+      const commandName = args.shift()?.toLowerCase();
 
-            if (!command) {
-              return;
-            }
-      
-            try {
-              await command.execute(message, client, args);
-            } catch (error) {
-              console.error(error);
-              message.reply(`${(error as Error).message}`);
-            }
-          }
 
-        const randomAmountOfXp = Math.floor(Math.random() * 50) + 1;;
+      if (!commandName) {
+        return;
+      }
 
-        const hasLeveledUp = await Levels.appendXp(message.member?.id, message.guild?.id, randomAmountOfXp);
+      const command = client.messageCommands.get(commandName);
 
-        if (hasLeveledUp) {
-            const user = await Levels.fetch(message.member?.id, message.guild?.id);
-            const LevelRoles = await LevelRolesModel.find({})
 
-            const member = message.guild?.members.cache.get(message.member?.id as string);
+      if (!command) {
+        return;
+      }
 
-            const levelUpEmbed = new EmbedBuilder()
-                .setColor(0x0099FF)
-                .setTitle("Level Up!")
-                .setDescription(DEFUALT_LEVEL_UP_MESSAGE.replace("%u", `<@${message.member?.id}>`).replace("%l", user.level.toString()));
-
-            await message.channel.send({ embeds: [levelUpEmbed] });
-
-            for (const role of LevelRoles) {
-                if (user.level.toString() >= role.neededLevel.toString()) {
-                    member?.roles.add(role.role);
-                } else {
-                    console.log(`User ${message.member?.id} has not reached the level ${role.neededLevel}`);
-                }
-            }
-        }
+      try {
+        await command.execute(message, client, args);
+      } catch (error) {
+        console.error(error);
+        message.reply(`${(error as Error).message}`);
+      }
     }
+
+    const randomAmountOfXp = Math.floor(Math.random() * 50) + 1;;
+
+    const hasLeveledUp = await Levels.appendXp(message.member?.id, message.guild?.id, randomAmountOfXp);
+
+    if (hasLeveledUp) {
+      const user = await Levels.fetch(message.member?.id, message.guild?.id);
+      const LevelRoles = await LevelRolesModel.find({})
+
+      const member = message.guild?.members.cache.get(message.member?.id as string);
+
+      const levelUpEmbed = new EmbedBuilder()
+        .setColor(0x0099FF)
+        .setTitle("Level Up!")
+        .setDescription(DEFUALT_LEVEL_UP_MESSAGE.replace("%u", `<@${message.member?.id}>`).replace("%l", user.level.toString()));
+
+
+
+      for (const role of LevelRoles) {
+        if (user.level.toString() >= role.neededLevel.toString()) {
+          member?.roles.add(role.role);
+        } else {
+          console.log(`User ${message.member?.id} has not reached the level ${role.neededLevel}`);
+        }
+      }
+
+      await message.channel.send({ embeds: [levelUpEmbed] }).then((msg: Message) => {
+        setTimeout(() => {
+          msg.delete();
+        }, 5000);
+      })
+    }
+  }
 }
